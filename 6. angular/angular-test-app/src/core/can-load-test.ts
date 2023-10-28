7286ebfc298bcc035db06af9d2b9dc65c5db9d47
Tamas Bun <tamas.bun@casrd.net>
2023-10-28 10:27:31 +0200

    Angular update

--- 6. angular/angular-test-app/src/core/can-load-test.ts
+++ 6. angular/angular-test-app/src/core/can-load-test.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import { CanLoad, Route, UrlSegment, UrlTree } from '@angular/router';
+import { Route, UrlSegment, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable()
-export class CanLoadTest implements CanLoad {
+export class CanLoadTest  {
 
     canLoad(route: Route, segments: UrlSegment[]): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
         return true;
